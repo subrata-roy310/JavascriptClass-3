e98d800b5d6251e2e8870514b43bcf4f63871aaa
subrata410 <subrata.rooy2022@gmail.com>
2022-12-22 15:41:08 +0600

    JavaScript Class Three

---
+++ script.js
@@ -0,0 +1,101 @@
+// // 1. Comparison Operators
+// // ==
+// console.log(9 == 9);
+// console.log(9 == "9");
+
+// // ===
+// console.log(9 === "9")
+
+// // !=
+// console.log(9 != 9);
+
+// // !== not equal value or not equal type
+// console.log(9 !== "9");
+
+// // >
+// console.log(9 > 8);
+// // <
+// console.log(9 < 8);
+// // >=
+// console.log(9 >= 9);
+// // <=
+// console.log(9 <= 10);
+// // ? ternary Operator
+
+
+// 2. Logical opertators
+//  &&
+// ||
+// !
+//Data && Data
+// let x = 6;
+// let y = 5;
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 && y > 3);
+
+// let x = 6;
+// let y = 2;
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 && y > 3);
+
+// // ||
+// console.log(x < 9 || y > 3)
+
+// // !
+// console.log(x == y);
+// console.log(!(x == y));
+
+
+// 3. Operator Precedence
+// B O D M A S
+// console.log((36 / 6) * 3 + 2 ** 2 - (3 + 5))
+// // 6 * 3 + 2 ** 2 - 8
+// // 6 * 3 + 4 - 8
+// // 18 + 4 -8
+// // 22 -8
+// // 18
+
+// 4. Template Literals
+// const fistName = "Subrata";
+// const lastName = "Roy";
+// let age = 24;
+// let job = "Web developer";
+// const country = "Bangladesh";
+// // I'm Subrata Roy, 24 years old Web developer!
+// // console.log("I'm Subrata Roy, 24 years old Web developer!");
+// // console.log("I'm" + " " + fistName + " " + lastName + "," + " " + age + " " + "years old" + " " + job + "!")
+// console.log(`I'm ${fistName} ${lastName}, ${age} years old ${job}!`)
+// console.log(`I love ${country}!`)
+
+// console.log(`First line
+// Second line
+// third line
+// fourth line`);
+
+// console.log(`Bangledesh win ${2 + 1} series`);
+
+//5. If / Else Statements
+// if(){
+
+// } else{
+
+// }
+// if (5 > 6) {
+//     console.log("5")
+// } else {
+//     console.log("Something else!");
+// }
+// if (10 < 15) {
+//     console.log("Display 10");
+// } else {
+//     console.log("Somethings else!")
+// }
+
+// let nituAge = 5;
+// if (nituAge >= 6) {
+//     console.log("Nitu can stsrt school!");
+// } else {
+//     console.log("Nitu can't start school!")
+// };
